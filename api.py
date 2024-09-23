ec32b47acb537ae84cea583b02f993b252754e3c
noefforted <ahmadopa73@gmail.com>
2024-09-23 17:11:33 +0700

    ke api tapi masih dict

---
+++ api.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import pandas as pd
+
+app = FastAPI()
+
+fuel_data_storage = pd.DataFrame()
+
+
+class FuelData(BaseModel):
+    Cycle: int
+    Fuel_Consumed: float
+    Fuel_Rate: float
+    Time_Awal: int
+    Time_Akhir: int
+
+
+@app.post("/fuel-rate", status_code=201)
+def post_fuel_rate(data: List[FuelData]):
+    global fuel_data_storage
+
+    # Konversi list dari Pydantic model ke DataFrame
+    data_df = pd.DataFrame([item.dict() for item in data])
+
+    if fuel_data_storage.empty:
+        fuel_data_storage = data_df
+    else:
+        fuel_data_storage = pd.concat(
+            [fuel_data_storage, data_df], ignore_index=True)
+
+    return {"message": "Data berhasil diterima", "data_count": len(data)}
+
+
+@app.get("/fuel-rate", response_model=List[FuelData])
+def get_fuel_rate():
+    global fuel_data_storage
+
+    if fuel_data_storage.empty:
+        raise HTTPException(status_code=404, detail="Data tidak ditemukan")
+
+    # Mengubah DataFrame menjadi list of dictionaries untuk dikembalikan sebagai response
+    return fuel_data_storage.to_dict(orient='records')
--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import data_processing
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from datetime import datetime, timedelta
+import requests
+import json
 
 now = datetime.now()
 delta = now - timedelta(days=3)
+
+url = "http://127.0.0.1:8000/fuel-rate"
 
 
 def main():
@@ -32,11 +36,22 @@ def main():
 
     print(f"{final_data}\n")
 
+    dict_final_data = final_data.to_dict(orient='records')
+
+    response = requests.post(url, json=dict_final_data)
+
+    if response.status_code == 201:
+        print("Data terkirim")
+    else:
+        print(f"Data gagal terkirim, status: {response.status_code}")
+        print(f"Response: ", response.text)
+
 
 scheduler = BackgroundScheduler()
 
 # Menambahkan pekerjaan dengan interval-based (setiap 5 detik)
-scheduler.add_job(main, 'interval', minutes=30)
+# scheduler.add_job(main, 'interval', minutes=30)
+scheduler.add_job(main, 'interval', seconds=5)
 
 # Memulai scheduler
 scheduler.start()
